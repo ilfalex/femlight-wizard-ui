51ce8c3b070b77f3f914c123a63e1efcc12e5aca
Alexander Mayo <mayoalexandertd@icloud.com>
2019-07-15 15:32:51 -0500

    installed dependencies

---
+++ src/components/Page/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <swiper-container/>
+</template>
+
+<script>
+import SwiperContainer from '../SwiperContainer'
+export default {
+    components: {
+      SwiperContainer
+    },
+    data: function () {
+        return {
+           message: 'Hello I am talking from the example component!'
+        }
+    },
+    mounted() {
+        console.log('Example Component mounted.')
+    }
+}
+</script>
---
+++ src/components/Steps/UploadInsignia.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+
+        This is where you upload your Insignia
+
+				<drop-zone
+					ref="myVueDropzone"
+					id="upload-insignia"
+					:options="dropzoneOptions" />
+
+	</div>
+</template>
+
+<script>
+	import DropZone from 'vue2-dropzone'
+	import 'vue2-dropzone/dist/vue2Dropzone.min.css'
+	export default {
+		props: {
+		},
+		data: function () {
+	    return {
+	      dropzoneOptions: {
+	          url: 'https://httpbin.org/post',
+	          thumbnailWidth: 150,
+	          maxFilesize: 0.5,
+	          headers: { "My-Awesome-Header": "header value" }
+	      }
+	    }
+	  },
+		mounted () {},
+		components: {
+			DropZone
+		},
+		methods: {}
+	}
+</script>
